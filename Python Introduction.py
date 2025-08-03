467e630dcd97c68a9dcb5e210a1282f017bb8b88
Rodah-chep <113697503+Rodah-chep@users.noreply.github.com>
2025-08-03 19:04:52 +0300

    Update Python Introduction.py

--- Python Introduction.py
+++ Python Introduction.py
@@ -1,1 +1,46 @@
+site_name = "Power Learn Project"
+print(site_name)
+
+# Assigning new value to site_name
+site_name = "I love coding 😊"
+print(site_name)
+
+# Variable assignment example
+# Variables can hold different types of data
+a,b,c = 5, 7, "Hello world"
+print(a) # prints 5
+print(b) # prints 5
+print(c) # prints Hello World
+
+# List example
+# Lists are mutable, meaning they can be changed after creation
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+
+# Tuple example
+# Tuples are immutable, meaning they cannot be changed after creation
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+# Dictionary example
+# Dictionaries are mutable and store data in key-value pairs    
+products_dict = {
+    "name": "XBox",
+    "price": 499.99,
+    "description": "Habibi",
+    "quantity": 23
+}
+
+#Showing strings
+site_name = "Power Learn Project"
+print(site_name)
+
+Name = input("Enter your name: ")
+favorite_color = input("Enter your favorite color: ")
+print(f"Hello {Name}!, your favorite color is {favorite_color}.")
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+sum = int(num1) + int(num2)
+print("The sum is:", sum)
 
